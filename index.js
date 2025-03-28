f3e15ce9379419c5e36f7ee33b7a0aab71d7aed4
Zak Burke <zburke@ebsco.com>
2025-03-28 08:50:06 -0400

    always supply factoryWithThrowingShims
    In order to silence prop-types warnings in every env, always return
    `factoryWithThrowingShims` instead of switching on `NODE_ENV`.

--- index.js
+++ index.js
@@ -5,15 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-if (process.env.NODE_ENV !== 'production') {
-  var ReactIs = require('react-is');
-
-  // By explicitly using `prop-types` you are opting into new development behavior.
-  // http://fb.me/prop-types-in-prod
-  var throwOnDirectAccess = true;
-  module.exports = require('./factoryWithTypeCheckers')(ReactIs.isElement, throwOnDirectAccess);
-} else {
-  // By explicitly using `prop-types` you are opting into new production behavior.
-  // http://fb.me/prop-types-in-prod
-  module.exports = require('./factoryWithThrowingShims')();
-}
+module.exports = require('./factoryWithThrowingShims')();
